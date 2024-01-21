4add6ebb0bd909df1cd37e415c0e55e3fff2ad09
kyj91032 <sees77770@gmail.com>
2024-01-21 17:43:52 +0900

    workflow test3

--- inhaCarPool_SERVER/src/main/java/com/example/inhaCarpool/InhaCarpoolApplication.java
+++ inhaCarPool_SERVER/src/main/java/com/example/inhaCarpool/InhaCarpoolApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InhaCarpoolApplication {
+public class InhaCarpoolApplication { 
 
     public static void main(String[] args) {
         SpringApplication.run(InhaCarpoolApplication.class, args);
