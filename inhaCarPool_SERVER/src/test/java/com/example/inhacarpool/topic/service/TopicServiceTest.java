e0733188c135540f267a5dac914fde01fffa2cf5
Yeongjae Kim <sees77770@gmail.com>
2024-08-07 14:26:57 +0900

    test : initTopicService 메서드 구현1

---
+++ inhaCarPool_SERVER/src/test/java/com/example/inhacarpool/topic/service/TopicServiceTest.java
@@ -0,0 +1,14 @@
+package com.example.inhacarpool.topic.service;
+
+public class TopicServiceTest {
+
+    private TopicServiceImpl topicService;
+
+//    @BeforeEach
+//    void initTopicService() {
+//        this.topicService = new TopicServiceImpl(
+//                new FixedClockHolder(LocalDateTime.of(2000, 1, 1, 0, 0, 0)),
+//                //TODO : Fake TopicRepository 생성 필요
+//        );
+//    }
+}
